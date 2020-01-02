36a978cc4bf85e85b58b7d0a0bff68d0d18365f5
iPsych0 <daniel.oliemans@gmail.com>
2020-01-02 15:51:45 +0100

    Made Venovine colour a bit darker to make it more visible. Added melee to Malachite Thug Leader Devon.

--- src/dev/ipsych0/myrinnia/entities/creatures/MalachiteThugL.java
+++ src/dev/ipsych0/myrinnia/entities/creatures/MalachiteThugL.java
@@ -1,6 +1,7 @@
 package dev.ipsych0.myrinnia.entities.creatures;
 
 import dev.ipsych0.myrinnia.Handler;
+import dev.ipsych0.myrinnia.gfx.Animation;
 import dev.ipsych0.myrinnia.gfx.Assets;
 import dev.ipsych0.myrinnia.items.Item;
 import dev.ipsych0.myrinnia.pathfinding.AStarMap;
@@ -8,6 +9,7 @@ import dev.ipsych0.myrinnia.skills.SkillsList;
 import dev.ipsych0.myrinnia.tiles.Tile;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public class MalachiteThugL extends Creature {
 
@@ -19,6 +21,7 @@ public class MalachiteThugL extends Creature {
 
     //Attack timer
     private long lastAttackTimer, attackCooldown = 1200, attackTimer = attackCooldown;
+    private Animation meleeAnimation;
 
     public MalachiteThugL(double x, double y, int width, int height, String name, int level, String dropTable, String jsonFile, String animation, String itemsShop, Direction direction) {
         super(x, y, width, height, name, level, dropTable, jsonFile, animation, itemsShop, direction);
@@ -33,7 +36,6 @@ public class MalachiteThugL extends Creature {
         defence = 8;
         maxHealth = DEFAULT_HEALTH + vitality * 2;
         health = maxHealth;
-        attackRange = Tile.TILEWIDTH * 5;
 
         bounds.x = 2;
         bounds.y = 2;
@@ -49,6 +51,20 @@ public class MalachiteThugL extends Creature {
     public void render(Graphics2D g) {
         g.drawImage(getAnimationByLastFaced(), (int) (x - Handler.get().getGameCamera().getxOffset()), (int) (y - Handler.get().getGameCamera().getyOffset())
                 , width, height, null);
+
+        if (meleeAnimation != null) {
+            if (meleeAnimation.isTickDone()) {
+                meleeAnimation = null;
+            } else {
+                meleeAnimation.tick();
+
+                AffineTransform old = g.getTransform();
+                g.rotate(Math.toRadians(meleeDirection), (int) (x + meleeXOffset + width / 2 - Handler.get().getGameCamera().getxOffset()), (int) (y + meleeYOffset + height / 2 - Handler.get().getGameCamera().getyOffset()));
+                g.drawImage(meleeAnimation.getCurrentFrame(), (int) (x + meleeXOffset - Handler.get().getGameCamera().getxOffset()),
+                        (int) (y + meleeYOffset - Handler.get().getGameCamera().getyOffset()), (int) (width * 1.25f), (int) (height * 1.25f), null);
+                g.setTransform(old);
+            }
+        }
     }
 
     @Override
@@ -67,17 +83,15 @@ public class MalachiteThugL extends Creature {
         if (attackTimer < attackCooldown)
             return;
 
-//		// Set attack-box
-//		Rectangle cb = getCollisionBounds(0,0);
-//		Rectangle ar = new Rectangle();
-//		int arSize = Creature.DEFAULT_CREATURE_HEIGHT;
-//		ar.width = arSize;
-//		ar.height = arSize;
-
         attackTimer = 0;
 
-        Handler.get().playEffect("abilities/sword_swing.ogg");
-        new Projectile.Builder(DamageType.STR, Assets.waterProjectile, this, (int) Handler.get().getPlayer().getX(), (int) Handler.get().getPlayer().getY()).build();
+        meleeAnimation = new Animation(48, Assets.regularMelee, true, false);
+
+        setMeleeSwing(new Rectangle((int) Handler.get().getPlayer().getX(), (int) Handler.get().getPlayer().getY(), 1, 1));
+
+        Handler.get().playEffect("abilities/sword_swing.ogg", -0.05f);
+
+        checkMeleeHitboxes();
 
     }
 
